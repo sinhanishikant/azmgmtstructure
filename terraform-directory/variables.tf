de49a53bd19f44537a53a7c1fd52b16238cbb4b6
sinhanishikant <sinha.nishikant24@outlook.com>
2025-06-06 16:53:53 +0530

    Create variables.tf

---
+++ terraform-directory/variables.tf
@@ -0,0 +1,32 @@
+variable "root_management_group_id" {
+  description = "The ID of the root management group (usually your tenant ID or 'root')."
+  type        = string
+  default     = "1a4d2aaa-669c-4d02-a275-bd3716545470" # Replace with your actual root management group ID
+}
+
+variable "parent_management_group_name" {
+  description = "Name of the parent management group under the root."
+  type        = string
+  default     = "IMSRoot"
+}
+
+variable "child_management_groups" {
+  description = "A map of child management groups to be created under the parent management group."
+  type        = map(string)
+  default = {
+    "platform"   = "Platform services"
+    "environments"    = "Environments"
+    "sandbox"    = "Sandbox environments"
+    "decommissioned"   = "Decommissioned"
+  }
+}
+
+variable "tags" {
+  description = "Tags for all management groups."
+  type        = map(string)
+  default = {
+    environment = "management"
+    owner       = "IMS"
+    created_by  = "terraform"
+  }
+}
