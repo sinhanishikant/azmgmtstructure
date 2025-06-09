668aed76f6e230c623eeb6819fde0e1319cab9ae
sinhanishikant <sinha.nishikant24@outlook.com>
2025-06-09 17:35:04 +0530

    Update main.tf

--- terraform-directory/main.tf
+++ terraform-directory/main.tf
@@ -16,3 +16,19 @@ provider "azurerm" {
 }
 
 # Define the root management group under the tenant
+resource "azurerm_management_group" "ims_root" {
+  name      = var.parent_management_group_name
+  display_name = var.parent_management_group_name
+  parent_management_group_id = var.root_management_group_id # This is implicitly the tenant root
+}
+# Example usage of the management group resource ID (path):
+output "management_group_id" {
+  value = azurerm_management_group.ims_root.id
+}
+# Tier 1 Groups
+resource "azurerm_management_group" "tier1" {
+  for_each = var.child_management_groups
+  display_name = each.value
+  name         = each.key
+  parent_management_group_id = azurerm_management_group.ims_root.id
+}
