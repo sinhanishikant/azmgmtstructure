a58bcb27b27827ce56f95c9219de24048f17a8d6
sinhanishikant <sinha.nishikant24@outlook.com>
2025-06-20 16:34:39 +0530

    Create backend.tf

---
+++ modules/management-groups/backend.tf
@@ -0,0 +1,21 @@
+# This file defines the backend configuration for Terraform state management.
+# terraform {
+# backend "azurerm" {
+#    resource_group_name  = "stateresourcegroup"
+#    storage_account_name = "practicetfstorage"
+#    container_name       = "tfstate"
+#    key                  = "terraform.tfstate"
+#  }
+# }
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "${var.backend_resource_group_name}"
+    storage_account_name = "${var.backend_storage_account_name}"
+    container_name       = "${var.backend_container_name}"
+    key                  = "${var.backend_key}" # Path to the state file in the container
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
