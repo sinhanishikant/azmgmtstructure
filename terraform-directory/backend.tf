17587511b44e0aa2f2f0b8b4455497365cfb72e9
sinhanishikant <sinha.nishikant24@outlook.com>
2025-06-20 16:21:54 +0530

    Update backend.tf

--- terraform-directory/backend.tf
+++ terraform-directory/backend.tf
@@ -7,3 +7,15 @@ backend "azurerm" {
     key                  = "terraform.tfstate"
   }
 }
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "${var.backend_resource_group_name}"
+    storage_account_name = "${var.backend_storage_account_name}"
+    container_name       = "${var.backend_container_name}"
+    key                  = "${var.backend_key}" # Path to the state file in the container
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
