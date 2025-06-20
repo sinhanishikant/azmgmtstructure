c34e2197228eeeac41c167d8a3165c1a89847821
sinhanishikant <sinha.nishikant24@outlook.com>
2025-06-20 16:21:26 +0530

    Update main.tf

--- modules/management-groups/main.tf
+++ modules/management-groups/main.tf
@@ -1,12 +1,28 @@
 terraform {
-  backend "azurerm" {
-    resource_group_name  = "${var.backend_resource_group_name}"
-    storage_account_name = "${var.backend_storage_account_name}"
-    container_name       = "${var.backend_container_name}"
-    key                  = "${var.backend_key}" # Path to the state file in the container
+  required_version = ">= 1.0"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 3.0"
+    }
+    azuread = {
+      source  = "hashicorp/azuread"
+      version = ">= 2.0"
+    }
   }
 }
-
 provider "azurerm" {
   features {}
+}
+
+# Define the root management group under the tenant
+resource "azurerm_management_group" "ims_root" {
+  name      = var.parent_management_group_name
+  display_name = var.parent_management_group_name
+  parent_management_group_id = var.root_management_group_id # This is implicitly the tenant root
+}
+
+# Example usage of the management group resource ID (path):
+output "management_group_id" {
+  value = azurerm_management_group.ims_root.id
 }
