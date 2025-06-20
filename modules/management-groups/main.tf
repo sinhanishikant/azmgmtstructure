2bbc960c0b1d5a4495355e902dd344787d4c0534
sinhanishikant <sinha.nishikant24@outlook.com>
2025-06-20 20:22:55 +0530

    Update main.tf

--- modules/management-groups/main.tf
+++ modules/management-groups/main.tf
@@ -26,3 +26,49 @@ resource "azurerm_management_group" "ims-root" {
 output "management_group_id" {
   value = azurerm_management_group.ims-root.id
 }
+# Create tier 1 management groups
+resource "azurerm_management_group" "platform" {
+  name        = "platform"
+  display_name = "platform"
+  parent_management_group_id = var.root_management_group_id # Use the root management group ID directly
+}
+
+resource "azurerm_management_group" "environments" {
+  name        = "environments"
+  display_name = "environments"
+  parent_management_group_id = var.root_management_group_id # Use the root management group ID directly
+}
+resource "azurerm_management_group" "sandbox" {
+  name        = "ims-sandbox"
+  display_name = "ims-sandbox"
+  parent_management_group_id = var.root_management_group_id # Use the root management group ID directly
+}
+resource "azurerm_management_group" "decommissioned" {
+  name        = "ims-decommissioned"
+  display_name = "ims-decommissioned"
+  parent_management_group_id = var.root_management_group_id # Use the root management group ID directly
+}
+# Create tier 2 Management Groups
+resource "azurerm_management_group" "Development" {
+  name        = "ims-environments-development"
+  display_name = "ims-environments-development"
+  parent_management_group_id = azurerm_management_group.environments.id
+}
+
+resource "azurerm_management_group" "envproduction" {
+  name        = "ims-environments-production"
+  display_name = "ims-environments-production"
+  parent_management_group_id = azurerm_management_group.environments.id
+}
+
+resource "azurerm_management_group" "non_prod" {
+  name        = "ims-platform-nonprod"
+  display_name = "ims-platform-nonprod"
+  parent_management_group_id = azurerm_management_group.platform.id
+}
+
+resource "azurerm_management_group" "production" {
+  name        = "ims-platform-prod"
+  display_name = "ims-platform-production"
+  parent_management_group_id = azurerm_management_group.platform.id
+}
