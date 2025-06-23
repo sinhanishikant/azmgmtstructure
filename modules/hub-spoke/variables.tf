7a9ff5d606392164cd5eca46be025e21b52bcb85
sinhanishikant <sinha.nishikant24@outlook.com>
2025-06-23 18:07:24 +0530

    Update variables.tf

--- modules/hub-spoke/variables.tf
+++ modules/hub-spoke/variables.tf
@@ -1,14 +1,14 @@
-variable "root_management_group_id" {
-  description = "The ID of the root management group (usually your tenant ID or 'root')."
-  type        = string
-  default     = "/providers/Microsoft.Management/managementGroups/1a4d2aaa-669c-4d02-a275-bd3716545470" # Replace with your actual root management group ID
-}
+# variable "root_management_group_id" {
+#  description = "The ID of the root management group (usually your tenant ID or 'root')."
+#  type        = string
+#  default     = "/providers/Microsoft.Management/managementGroups/1a4d2aaa-669c-4d02-a275-bd3716545470" # Replace with your actual root management group ID
+# }
 
-variable "parent_management_group_name" {
-  description = "Name of the parent management group under the root."
-  type        = string
-  default     = "IMS-ROOT"
-}
+#variable "parent_management_group_name" {
+#  description = "Name of the parent management group under the root."
+#  type        = string
+#  default     = "IMS-ROOT"
+# }
 
 variable "location" { 
   description = "Azure region for resource deployment"
