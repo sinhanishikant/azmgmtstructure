4858f07a704e69581bf27ee4707a93dd8036ad6e
sinhanishikant <sinha.nishikant24@outlook.com>
2025-06-23 18:41:07 +0530

    Update backend.tf

--- modules/hub-spoke/backend.tf
+++ modules/hub-spoke/backend.tf
@@ -2,7 +2,7 @@
  backend "azurerm" {
     resource_group_name  = "stateresourcegroup"
     storage_account_name = "practicetfstorage"
-    container_name       = "tfstateoidc"
-    key                  = "terraform.tfstateoidc"
+    container_name       = "tfstatehub"
+    key                  = "terraform.tfstatehub"
   }
  }
